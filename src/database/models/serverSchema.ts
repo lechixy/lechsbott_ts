6a800262b370beda2b1f2b090f73eb1ce58f052b
Lechixy <51778131+Lechixy@users.noreply.github.com>
2021-12-02 23:23:08 +0300

    Delete src directory

--- src/database/models/serverSchema.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose'
-
-const serverSchema = new mongoose.Schema({
-    serverId: { type: String, required: true, unique: true },
-    prefix: { type: String, required: true },
-})
-
-export default mongoose.model("servers", serverSchema)
